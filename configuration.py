2d52ec1419682fec977d7e6d79888c0e0117ca55
Solntsev1993 <solntsev1993@gmail.com>
2024-02-25 18:36:45 +0300

    1

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://8bcc5519-f60d-4d61-8393-641a10855462.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://b79c2210-aa9d-4d7e-9547-b3191c82abc8.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 ORDER_LIST = "/api/v1/orders/track?t="
